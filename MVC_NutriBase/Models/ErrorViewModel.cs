0f8c4e1a03d643d7731404e4bf1c8aa1c238fba4
Sspect <bastian.stokke@gmail.com>
2024-09-23 15:28:45 +0200

    MVC and UI setup
    I created two directories where MVC is for one of the three tasks,(Sub-application 1) and UI is a directorie where we can make a ui without logic.

---
+++ MVC_NutriBase/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_NutriBase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
